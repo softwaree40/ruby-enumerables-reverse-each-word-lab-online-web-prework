07d8118307d6f034b90d9ddfe3017257b001d63f
relational-router-4343 <ajagunnaabiodun752@yahoo.com>
2019-10-10 03:39:59 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,3 +4,9 @@ def reverse_each_word(sentence1)
       words.each {|word| new_array << word.reverse}
       return new_array.join(' ')
 end
+def reverse_each_word(sentence1)
+      new_array = []
+      words = sentence1.split(' ')
+      words.collect {|word| new_array << word.reverse}
+      return new_array.join(' ')
+end
